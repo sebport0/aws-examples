53f8bf412491bf0da4df2437dee4029f9e98d02f
Samuel Bassi <sebassi@live.com>
2022-02-20 16:09:43 -0300

    Update lambda function to write a message to a dynamo table

--- hello-lambda-cdk-go/hello_function/main.go
+++ hello-lambda-cdk-go/hello_function/main.go
@@ -3,16 +3,53 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+
+	"github.com/google/uuid"
 )
 
 type MyEvent struct {
-	Name string `json:"name"`
+	Message string `json:"message"`
+}
+
+type MessageRecord struct {
+	ID      string
+	MESSAGE string
 }
 
 func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
-	return fmt.Sprintf("I'm alive. Hello %s!", event.Name), nil
+	// New dynamoDB client.
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		panic("Unable to load SDK config, " + err.Error())
+	}
+	dynamoClient := dynamodb.NewFromConfig(cfg)
+
+	tableName := os.Getenv("TABLE")
+	messageRecord := MessageRecord{
+		ID:      uuid.NewString(),
+		MESSAGE: event.Message,
+	}
+	item, err := attributevalue.MarshalMap(messageRecord)
+	if err != nil {
+		panic(fmt.Sprintf("Something went wrong with DynamoDB marshal %v", err))
+	}
+
+	_, err = dynamoClient.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item:      item,
+	})
+	if err != nil {
+		panic(fmt.Sprintf("Something went wrong with DynamoDB putItem operation, %v", err))
+	}
+
+	return "Ok", nil
 }
 
 func main() {
