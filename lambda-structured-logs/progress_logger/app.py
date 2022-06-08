ff77a969156506427e7e467cc7a777f2761f8aa8
Samuel Bassi <sebassi@live.com>
2022-06-07 21:57:25 -0300

    Update progress logger to show nested field example

--- lambda-structured-logs/progress_logger/app.py
+++ lambda-structured-logs/progress_logger/app.py
@@ -7,15 +7,15 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     logger.info({"state": "PENDING", "failed": False})
     logger.info({"state": "INITIATED", "failed": False})
-    logger.info({"state": "RUNNING", "failed": False, "progress": 0})
-    logger.info({"state": "RUNNING", "failed": False, "progress": 25})
-    logger.info({"state": "RUNNING", "failed": False, "progress": 50})
-    logger.error({"state": "TERMINATING", "failed": True, "progress": 75})
+    logger.info({"state": "RUNNING", "failed": False, "progress": {"at": 0, "remaining": 100}})
+    logger.info({"state": "RUNNING", "failed": False, "progress": {"at": 25, "remaining": 75}})
+    logger.info({"state": "RUNNING", "failed": False, "progress": {"at": 50, "remaining": 50}})
+    logger.error({"state": "TERMINATING", "failed": True, "progress": {"at": 75, "remaining": 25}})
     logger.info(
         {
             "state": "TERMINATED",
             "failed": True,
-            "progress": 75,
+            "progress": {"at": 75, "remaining": 25},
             "message": "Task failed at 75%. Unable to upload images.",
         }
     )
